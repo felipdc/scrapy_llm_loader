88ddcd108a943e01d08852595658408c85286c44
Felipe <felipetdecarli@gmail.com>
2023-11-13 12:06:39 -0300

    version: update to 0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='scrapy_llm_loader',
-    version='0.1',
+    version='0.2',
     description='A Scrapy extension for data extraction using LangChain',
     packages=find_packages()
 )
